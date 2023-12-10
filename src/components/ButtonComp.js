31b95ad63da88fc44338ac3643cca47c146d702b
Nizam_V <nizamvali1104@gmail.com>
2023-12-10 09:35:18 +0530

    project setup

---
+++ src/components/ButtonComp.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ButtonComp() {
+  return (
+    <button className="bg-blue-500 rounded-md text-white px-10 py-2">
+      Click
+    </button>
+  );
+}
+
+export default ButtonComp;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import ButtonComp from "./components/ButtonComp";
+import MyButton from "./components/counter";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ButtonComp />
+    <MyButton />
   </React.StrictMode>
 );
 
